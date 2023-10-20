e6dbb62c1b80ff17e41d183a7f35e1c6c0366863
Johan Blumenberg <johan@truid.app>
2023-10-20 12:28:45 +0200

    Patch CloudSqlInstance

--- gcp-cloud-sql-serverless-patch/src/test/java/com/google/cloud/sql/core/CloudSqlInstanceTest.java
+++ gcp-cloud-sql-serverless-patch/src/test/java/com/google/cloud/sql/core/CloudSqlInstanceTest.java
@@ -7,14 +7,20 @@ import com.google.cloud.sql.CredentialFactory;
 import com.google.common.util.concurrent.AbstractFuture;
 import com.google.common.util.concurrent.ListenableFuture;
 import com.google.common.util.concurrent.ListeningScheduledExecutorService;
+import com.google.common.util.concurrent.MoreExecutors;
+import com.google.common.util.concurrent.RateLimiter;
+import com.google.common.util.concurrent.testing.TestingExecutors;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
 
 import java.security.KeyPair;
 import java.util.Date;
+import java.util.concurrent.Executors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -25,98 +31,102 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
 public class CloudSqlInstanceTest {
     private final String connectionName = "project:region:instance";
 
     @Mock
-    private SqlAdminApiFetcher apiFetcher;
+    private InstanceDataSupplier instanceDataSupplier;
 
     @Mock
     private CredentialFactory tokenSourceFactory;
 
-    @Mock
-    private ListeningScheduledExecutorService executor;
+    private ListeningScheduledExecutorService executor = TestingExecutors.sameThreadScheduledExecutor();
 
     @Mock
     private ListenableFuture<KeyPair> keyPair;
+
+    @Mock
+    private RateLimiter rateLimiter;
 
     @BeforeEach
     public void setup() {
         when(tokenSourceFactory.create()).thenReturn(
             new Credential.Builder(BearerToken.authorizationHeaderAccessMethod()).build()
         );
+        when(rateLimiter.acquire()).thenReturn(0.0);
     }
 
     @Test
     public void shouldBeAbleToFetchData() throws Exception {
         SslData sslData = createSslData();
-        when(apiFetcher.getInstanceData(any(), any(), eq(AuthType.IAM), any(), any())).thenReturn(
-            new ResolvedFuture(validInstanceData(sslData))
+        when(instanceDataSupplier.getInstanceData(any(), any(), eq(AuthType.IAM), any(), any())).thenReturn(
+            validInstanceData(sslData)
         );
 
-        CloudSqlInstance inst = new CloudSqlInstance(connectionName, apiFetcher, AuthType.IAM, tokenSourceFactory, executor, keyPair);
+        CloudSqlInstance inst = new CloudSqlInstance(connectionName, instanceDataSupplier, AuthType.IAM, tokenSourceFactory, executor, keyPair, rateLimiter);
         SslData result = inst.getSslData();
         assertEquals(sslData, result);
-        verify(apiFetcher, times(1)).getInstanceData(any(), any(), eq(AuthType.IAM), any(), any());
+        verify(instanceDataSupplier, times(1)).getInstanceData(any(), any(), eq(AuthType.IAM), any(), any());
     }
 
     @Test
     public void failOnStartupShouldRetryOnce() throws Exception {
         SslData sslData = createSslData();
-        when(apiFetcher.getInstanceData(any(), any(), eq(AuthType.IAM), any(), any()))
-            .thenReturn(new RejectedFuture(new RuntimeException("knas")))
-            .thenReturn(new ResolvedFuture(validInstanceData(sslData)));
+        when(instanceDataSupplier.getInstanceData(any(), any(), eq(AuthType.IAM), any(), any()))
+            .thenThrow(new RuntimeException("knas"))
+            .thenReturn(validInstanceData(sslData));
 
-        CloudSqlInstance inst = new CloudSqlInstance(connectionName, apiFetcher, AuthType.IAM, tokenSourceFactory, executor, keyPair);
+        CloudSqlInstance inst = new CloudSqlInstance(connectionName, instanceDataSupplier, AuthType.IAM, tokenSourceFactory, executor, keyPair, rateLimiter);
         SslData result = inst.getSslData();
         assertEquals(sslData, result);
-        verify(apiFetcher, times(2)).getInstanceData(any(), any(), eq(AuthType.IAM), any(), any());
+        verify(instanceDataSupplier, times(2)).getInstanceData(any(), any(), eq(AuthType.IAM), any(), any());
     }
 
     @Test
     public void failOnRefreshShouldRetryOnce() throws Exception {
         SslData sslData = createSslData();
-        when(apiFetcher.getInstanceData(any(), any(), eq(AuthType.IAM), any(), any()))
-            .thenReturn(new ResolvedFuture(expiredInstanceData()))
-            .thenReturn(new RejectedFuture(new RuntimeException("knas")))
-            .thenReturn(new RejectedFuture(new RuntimeException("knas")))
-            .thenReturn(new ResolvedFuture(validInstanceData(sslData)));
+        when(instanceDataSupplier.getInstanceData(any(), any(), eq(AuthType.IAM), any(), any()))
+            .thenReturn(expiredInstanceData())
+            .thenThrow(new RuntimeException("knas"))
+            .thenThrow(new RuntimeException("knas"))
+            .thenReturn(validInstanceData(sslData));
 
-        CloudSqlInstance inst = new CloudSqlInstance(connectionName, apiFetcher, AuthType.IAM, tokenSourceFactory, executor, keyPair);
+        CloudSqlInstance inst = new CloudSqlInstance(connectionName, instanceDataSupplier, AuthType.IAM, tokenSourceFactory, executor, keyPair, rateLimiter);
         assertThrows(RuntimeException.class, inst::getSslData);
         SslData result = inst.getSslData();
         assertEquals(sslData, result);
-        verify(apiFetcher, times(4)).getInstanceData(any(), any(), eq(AuthType.IAM), any(), any());
+        verify(instanceDataSupplier, times(4)).getInstanceData(any(), any(), eq(AuthType.IAM), any(), any());
     }
 
     @Test
     public void failTwiceOnStartupShouldRetryOnNextRefresh() throws Exception {
         SslData sslData = createSslData();
-        when(apiFetcher.getInstanceData(any(), any(), eq(AuthType.IAM), any(), any()))
-            .thenReturn(new RejectedFuture(new RuntimeException("knas")))
-            .thenReturn(new RejectedFuture(new RuntimeException("knas")))
-            .thenReturn(new ResolvedFuture(validInstanceData(sslData)));
+        when(instanceDataSupplier.getInstanceData(any(), any(), eq(AuthType.IAM), any(), any()))
+            .thenThrow(new RuntimeException("knas"))
+            .thenThrow(new RuntimeException("knas"))
+            .thenReturn(validInstanceData(sslData));
 
-        CloudSqlInstance inst = new CloudSqlInstance(connectionName, apiFetcher, AuthType.IAM, tokenSourceFactory, executor, keyPair);
+        CloudSqlInstance inst = new CloudSqlInstance(connectionName, instanceDataSupplier, AuthType.IAM, tokenSourceFactory, executor, keyPair, rateLimiter);
         SslData result = inst.getSslData();
         assertEquals(sslData, result);
-        verify(apiFetcher, times(3)).getInstanceData(any(), any(), eq(AuthType.IAM), any(), any());
+        verify(instanceDataSupplier, times(3)).getInstanceData(any(), any(), eq(AuthType.IAM), any(), any());
     }
 
     @Test
     public void failTwiceOnRefreshShouldRetryOnNextRefresh() throws Exception {
         SslData sslData = createSslData();
-        when(apiFetcher.getInstanceData(any(), any(), eq(AuthType.IAM), any(), any()))
-            .thenReturn(new ResolvedFuture(expiredInstanceData()))
-            .thenReturn(new RejectedFuture(new RuntimeException("knas")))
-            .thenReturn(new RejectedFuture(new RuntimeException("knas")))
-            .thenReturn(new ResolvedFuture(validInstanceData(sslData)));
+        when(instanceDataSupplier.getInstanceData(any(), any(), eq(AuthType.IAM), any(), any()))
+            .thenReturn(expiredInstanceData())
+            .thenThrow(new RuntimeException("knas"))
+            .thenThrow(new RuntimeException("knas"))
+            .thenReturn(validInstanceData(sslData));
 
-        CloudSqlInstance inst = new CloudSqlInstance(connectionName, apiFetcher, AuthType.IAM, tokenSourceFactory, executor, keyPair);
+        CloudSqlInstance inst = new CloudSqlInstance(connectionName, instanceDataSupplier, AuthType.IAM, tokenSourceFactory, executor, keyPair, rateLimiter);
         assertThrows(RuntimeException.class, inst::getSslData);
         SslData result = inst.getSslData();
         assertEquals(sslData, result);
-        verify(apiFetcher, times(4)).getInstanceData(any(), any(), eq(AuthType.IAM), any(), any());
+        verify(instanceDataSupplier, times(4)).getInstanceData(any(), any(), eq(AuthType.IAM), any(), any());
     }
 
     static class ResolvedFuture extends AbstractFuture<InstanceData> {
